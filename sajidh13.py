c4ff3b38252aae830e7eb25b2974c75309afd76e
SALMAN9499 <46928230+SALMAN9499@users.noreply.github.com>
2019-01-28 07:13:58 -0500

    Create sajidh13.py

---
+++ sajidh13.py
@@ -0,0 +1,13 @@
+#PRIME NUMBER OR NOT_SAJIN
+
+N=int(input())
+
+if N>1:
+    for i in range(2,N):
+        if (N%i==0):
+            print("no")
+            break
+    else:
+        print("yes")
+else:
+    print("no")
